2a60f4442cc2914cf8f20ec4c7753ca72dfc4ba5
mester <mestervalera@gmail.com>
2018-11-03 09:09:43 +0300

    Rinat

--- HelloWorld.java
+++ HelloWorld.java
@@ -16,7 +16,7 @@ g.setColor(Color.black);
 g.setFont(f);
 drawCenteredString("17130169"
 		+ "17130899"
-		+ "133008"
+		+ "17130467"
 		+ "133009", d.width, d.height, g);
 g.drawRect(0,0,d.width-1,d.height-1);
 }
